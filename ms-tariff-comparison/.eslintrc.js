7ee51d0a291e1331175914ff68e0721982ef35a9
Victor Soares <victor.soares@live.it>
2021-07-19 19:27:40 -0300

    chore: lint file

---
+++ ms-tariff-comparison/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+  },
+  'rules': {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "new-cap": [2, {"capIsNewExceptions": ["Schema"]}],
+    "require-jsdoc": ["error", {
+      "require": {
+        "FunctionDeclaration": false,
+        "MethodDefinition": false,
+        "ClassDeclaration": false,
+        "ArrowFunctionExpression": false,
+        "FunctionExpression": false
+      }
+    }]
+  }
+};
