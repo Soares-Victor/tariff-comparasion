39e8b69a44cf9b587d095ae998ee0e103b4b2c76
Victor Soares <victor.soares@live.it>
2021-08-06 11:37:40 -0300

    chore: list all calculations

--- ui-common-v2/src/routes.js
+++ ui-common-v2/src/routes.js
@@ -1,15 +1,10 @@
 
 import Dashboard from "views/Dashboard.js";
-import Icons from "views/Icons.js";
-import Map from "views/Map.js";
-import Notifications from "views/Notifications.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import UserProfile from "views/UserProfile.js";
 import Products from "./views/services/tariff/Products";
 import CostCalculate from "./views/services/costs/CostCalculate";
 import CostUploadFileToProcess from "./views/services/costs/CostUploadFileToProcess";
 import CostListAllFilesToProcess from "./views/services/costs/CostListAllFilesToProcess";
+import CostListAllCalculation from "./views/services/costs/CostListAllCalculation";
 
 var routes = [
   {
@@ -48,45 +43,10 @@ var routes = [
     layout: process.env.PUBLIC_URL,
   },
   {
-    path: `/icons`,
-    name: "Icons",
-    icon: "tim-icons icon-atom",
-    component: Icons,
-    layout: process.env.PUBLIC_URL,
-  },
-  {
-    path: `${process.env.PUBLIC_URL}/map`,
-    name: "Map",
-    icon: "tim-icons icon-pin",
-    component: Map,
-    layout: process.env.PUBLIC_URL,
-  },
-  {
-    path: `${process.env.PUBLIC_URL}/notifications`,
-    name: "Notifications",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: process.env.PUBLIC_URL,
-  },
-  {
-    path: `${process.env.PUBLIC_URL}/user-profile`,
-    name: "User Profile",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: process.env.PUBLIC_URL,
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
-    layout: process.env.PUBLIC_URL,
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "tim-icons icon-align-center",
-    component: Typography,
+    path: `/listall-calculation`,
+    name: "Check All Calculations",
+    icon: "tim-icons icon-chart-bar-32",
+    component: CostListAllCalculation,
     layout: process.env.PUBLIC_URL,
   },
 ];
