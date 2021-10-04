0491d7ac27858e980b6c43769609ca183ef70344
Victor Soares <victor.soares@live.it>
2021-10-04 19:34:32 -0300

    gateway and keycloak

--- architecture/start.sh
+++ architecture/start.sh
@@ -5,6 +5,22 @@ alias aws-docker='docker run --network host --rm -ti -v ~/.aws:/root/.aws -v $(p
 aws-docker --version
 docker-compose up -d
 sleep 10
+DIR="$HOME/.aws"
+if [ ! -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Installing config files in ${DIR}..."
+  mkdir ${DIR}
+  echo "
+[default]
+region = us-east-1
+output = json
+    " >> ${DIR}/config
+  echo "
+[default]
+aws_access_key_id = 123
+aws_secret_access_key = 12345
+    " >> ${DIR}/credentials 
+fi
 aws-docker --endpoint-url=http://localhost:4566 s3 mb s3://projectsysvictor-tariff-toprocess
 aws-docker --endpoint-url=http://localhost:4566 s3 mb s3://projectsysvictor-user-photos
 docker build -t ui-common ../ui-common
